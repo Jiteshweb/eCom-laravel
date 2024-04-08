35e162de11edd7b796b0be15844d824d8b10228d
Jitesh <Jitesh@QDS-PHP-RITESH.QDS.COM>
2024-04-08 17:13:47 +0530

    login done

---
+++ database/seeders/User_seeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+class User_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name'=>'Ajay singh',
+            'email'=>'ajay@gmail.com',
+            "password"=>Hash::make('12345')
+        ]);
+    }
+}
